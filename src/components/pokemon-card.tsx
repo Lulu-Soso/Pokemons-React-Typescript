6a2990a734768a3d9e28623e51f17e71d4e7d515
Lulu-Soso <lucas.sokha@gmail.com>
2023-11-14 22:45:41 +0100

    add format-date and format-card in helpers folder

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,27 +1,35 @@
 import React, { FunctionComponent, useState } from "react";
 import Pokemon from "../models/pokemon";
 import "./pokemon-card.css";
+import formatDate from "../helpers/format-date";
+import formatType from "../helpers/format-type";
 
 type Props = {
   pokemon: Pokemon;
   borderColor?: string; // ? prop facultative, type chaîne de caractère
 };
 
-const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = "#009688" }) => {
+const PokemonCard: FunctionComponent<Props> = ({
+  pokemon,
+  borderColor = "#009688",
+}) => {
+  const [color, setColor] = useState<string>();
 
-    const [color, setColor] = useState<string>();
+  const showBorder = () => {
+    setColor(borderColor);
+  };
 
-    const showBorder = () => {
-        setColor(borderColor)
-    }
-
-    const hideBorder = () => {
-        setColor("#f5f5f5") // On remet la bordure en gris
-    }
+  const hideBorder = () => {
+    setColor("#f5f5f5"); // On remet la bordure en gris
+  };
 
   return (
-    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
-      <div className="card horizontal" style={{ borderColor: color}}>
+    <div
+      className="col s6 m4"
+      onMouseEnter={showBorder}
+      onMouseLeave={hideBorder}
+    >
+      <div className="card horizontal" style={{ borderColor: color }}>
         <div className="card-image">
           <img src={pokemon.picture} alt={pokemon.name} />
         </div>
@@ -29,8 +37,13 @@ const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = "#009688
           <div className="card-content">
             <p>{pokemon.name}</p>
             <p>
-              <small>{pokemon.created.toString()}</small>
+              <small>{formatDate(pokemon.created)}</small>
             </p>
+            {pokemon.types.map((type) => (
+              <span key={type} className={formatType(type)}>
+                {type}
+              </span>
+            ))}
           </div>
         </div>
       </div>
